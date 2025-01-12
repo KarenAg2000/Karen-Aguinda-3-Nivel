9968c93bcaf16ca1400314a49ac43a7fc2bdceb3
KarenAg2000 <jaelakaren2000@gmail.com>
2025-01-11 22:26:44 -0500

    version inicial

--- Estructura de Datos/obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
+++ Estructura de Datos/obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc3fa2dbd15e1549db51612d96b0cd82a28aed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
